d9f374e14d1e22f367a0e4995ff503cb8d8c2be8
Russell Jurney <russell.jurney@gmail.com>
2020-02-24 05:04:33 -0800

    Working XML.Bzip2 to Parquet script for Stack Overflow data, with updated paths.json to go with it. Now removing _ prefix from attribute fields with remove_prefix(df).

--- ch02/xml_to_parquet.spark.py
+++ ch02/xml_to_parquet.spark.py
@@ -8,6 +8,7 @@
 import json
 
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
 
 
 # Initialize PySpark
@@ -15,10 +16,25 @@ spark = SparkSession.builder.appName('Weakly Supervised Learning - Convert XML t
 sc = spark.sparkContext
 
 # Load the many paths from a JSON file
+PATH_SET = 's3'
 PATHS = json.load(
     open('paths.json')
 )
-PATH_SET = 's3' # 'local'
+
+
+def remove_prefix(df):
+    """Remove the _ prefix that Spark-XML adds to all attributes"""
+    field_names = [x.name for x in df.schema]
+    new_field_names = [x[1:] for x in field_names]
+    s = []
+
+    # Substitute the old name for the new one
+    for old, new in zip(field_names, new_field_names):
+        s.append(
+            F.col(old).alias(new)
+        )
+    return df.select(s)
+
 
 # Use Spark-XML to split the XML file into records
 posts_df = spark.read.format('xml')\
@@ -26,10 +42,14 @@ posts_df = spark.read.format('xml')\
                 .options(rootTag='posts')\
                 .load(PATHS['posts_xml'][PATH_SET])
 
+# Remove the _ prefix from field names
+posts_df = remove_prefix(posts_df)
+
 # Write the DataFrame out to Parquet format
 posts_df.write\
         .mode('overwrite')\
         .parquet(PATHS['posts'][PATH_SET])
+
 
 # Use Spark-XML to split the XML file into records
 users_df = spark.read.format('xml')\
@@ -37,43 +57,70 @@ users_df = spark.read.format('xml')\
                 .options(rootTag='users')\
                 .load(PATHS['users_xml'][PATH_SET])
 
+# Remove the _ prefix from field names
+users_df = remove_prefix(users_df)
+
 # Write the DataFrame out to Parquet format
 users_df.write\
         .mode('overwrite')\
         .parquet(PATHS['users'][PATH_SET])
 
+
+# Use Spark-XML to split the XML file into records
 tags_df = spark.read.format('xml')\
                .options(rowTag='row')\
                .options(rootTag='tags')\
                .load(PATHS['tags_xml'][PATH_SET])
 
+# Remove the _ prefix from field names
+tags_df = remove_prefix(tags_df)
+
+# Write the DataFrame out to Parquet format
 tags_df.write\
        .mode('overwrite')\
        .parquet(PATHS['tags'][PATH_SET])
 
+
+# Use Spark-XML to split the XML file into records
 badges_df = spark.read.format('xml')\
                       .options(rowTag='row')\
                       .options(rootTag='badges')\
                       .load(PATHS['badges_xml'][PATH_SET])
 
+# Remove the _ prefix from field names
+badges_df = remove_prefix(badges_df)
+
+# Write the DataFrame out to Parquet format
 badges_df.write\
          .mode('overwrite')\
          .parquet(PATHS['badges'][PATH_SET])
 
+
+# Use Spark-XML to split the XML file into records
 comments_df = spark.read.format('xml')\
                         .options(rowTag='row')\
                         .options(rootTag='comments')\
                         .load(PATHS['comments_xml'][PATH_SET])
 
+# Remove the _ prefix from field names
+comments_df = remove_prefix(comments_df)
+
+# Write the DataFrame out to Parquet format
 comments_df.write\
            .mode('overwrite')\
            .parquet(PATHS['comments'][PATH_SET])
 
+
+# Use Spark-XML to split the XML file into records
 post_links_df = spark.read.format('xml')\
                      .options(rowTag='row')\
                      .options(rootTag='postlinks')\
                      .load(PATHS['postlinks_xml'][PATH_SET])
 
+# Remove the _ prefix from field names
+post_links_df = remove_prefix(post_links_df)
+
+# Write the DataFrame out to Parquet format
 post_links_df.write\
              .mode('overwrite')\
              .parquet(PATHS['postlinks'][PATH_SET])
