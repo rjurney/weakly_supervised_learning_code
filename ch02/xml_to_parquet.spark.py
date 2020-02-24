4255026ce23391a6a4321703cf25aec5780750df
Russell Jurney <russell.jurney@gmail.com>
2020-02-23 18:22:10 -0800

    Updated paths.json to use 2019-12-02 data, cleaned up xml_to_parquet.spark.py and converting everything

--- ch02/xml_to_parquet.spark.py
+++ ch02/xml_to_parquet.spark.py
@@ -2,7 +2,7 @@
 
 #
 # Convert the Stack Overflow data from XML format to Parquet format for performance reasons.
-# Run me with: PYSPARK_PYTHON=python3 PYSPARK_DRIVER_PYTHON=ipython3 pyspark/spark-submit --packages com.databricks:spark-xml_2.11:0.7.0
+# Run me with: PYSPARK_PYTHON=python3 PYSPARK_DRIVER_PYTHON=ipython3 pyspark/spark-submit --packages com.databricks:spark-xml_2.11:0.8.0
 #
 
 import json
@@ -10,48 +10,70 @@ import json
 from pyspark.sql import SparkSession
 
 
+# Initialize PySpark
 spark = SparkSession.builder.appName('Weakly Supervised Learning - Convert XML to Parquet').getOrCreate()
 sc = spark.sparkContext
-
-PATH_SET = 's3' # 'local'
 
 # Load the many paths from a JSON file
 PATHS = json.load(
     open('paths.json')
 )
+PATH_SET = 's3' # 'local'
 
-# Spark-XML DataFrame method
-posts_df = spark.read.format('xml').options(rowTag='row').options(rootTag='posts')\
+# Use Spark-XML to split the XML file into records
+posts_df = spark.read.format('xml')\
+                .options(rowTag='row')\
+                .options(rootTag='posts')\
                 .load(PATHS['posts_xml'][PATH_SET])
-posts_df.write.mode('overwrite')\
+
+# Write the DataFrame out to Parquet format
+posts_df.write\
+        .mode('overwrite')\
         .parquet(PATHS['posts'][PATH_SET])
 
-users_df = spark.read.format('xml').options(rowTag='row').options(rootTag='users')\
+# Use Spark-XML to split the XML file into records
+users_df = spark.read.format('xml')\
+                .options(rowTag='row')\
+                .options(rootTag='users')\
                 .load(PATHS['users_xml'][PATH_SET])
-users_df.write.mode('overwrite')\
+
+# Write the DataFrame out to Parquet format
+users_df.write\
+        .mode('overwrite')\
         .parquet(PATHS['users'][PATH_SET])
 
-# tags_df = spark.read.format('xml').options(rowTag='row').options(rootTag='tags')\
-#                .load('s3://stackoverflow-events/06-24-2019/Tags.xml.lzo')
-# tags_df.write.mode('overwrite')\
-#        .parquet('s3://stackoverflow-events/06-24-2019/Tags.df.parquet')
+tags_df = spark.read.format('xml')\
+               .options(rowTag='row')\
+               .options(rootTag='tags')\
+               .load(PATHS['tags_xml'][PATH_SET])
 
-# badges_df = spark.read.format('xml').options(rowTag='row').options(rootTag='badges')\
-#                  .load('s3://stackoverflow-events/06-24-2019/Badges.xml.lzo')
-# badges_df.write.mode('overwrite')\
-#          .parquet('s3://stackoverflow-events/06-24-2019/Badges.df.parquet')
+tags_df.write\
+       .mode('overwrite')\
+       .parquet(PATHS['tags'][PATH_SET])
 
-# comments_df = spark.read.format('xml').options(rowTag='row').options(rootTag='comments')\
-#                    .load('s3://stackoverflow-events/06-24-2019/Comments.xml.lzo')
-# comments_df.write.mode('overwrite')\
-#            .parquet('s3://stackoverflow-events/06-24-2019/Comments.df.parquet')
+badges_df = spark.read.format('xml')\
+                      .options(rowTag='row')\
+                      .options(rootTag='badges')\
+                      .load(PATHS['badges_xml'][PATH_SET])
 
-# history_df = spark.read.format('xml').options(rowTag='row')\
-#                   .options(rootTag='posthistory')\
-#                   .load('s3://stackoverflow-events/06-24-2019/PostHistory.xml.lzo')
-# history_df.write.mode('overwrite')\
-#           .parquet('s3://stackoverflow-events/06-24-2019/PostHistory.df.parquet')
-# votes_df = spark.read.format('xml').options(rowTag='row').options(rootTag='votes')\
-#                 .load('s3://stackoverflow-events/06-24-2019/Votes.xml.lzo')
-# votes_df.write.mode('overwrite')\
-#         .parquet('s3://stackoverflow-events/06-24-2019/Votes.df.parquet')
+badges_df.write\
+         .mode('overwrite')\
+         .parquet(PATHS['badges'][PATH_SET])
+
+comments_df = spark.read.format('xml')\
+                        .options(rowTag='row')\
+                        .options(rootTag='comments')\
+                        .load(PATHS['comments_xml'][PATH_SET])
+
+comments_df.write\
+           .mode('overwrite')\
+           .parquet(PATHS['comments'][PATH_SET])
+
+post_links_df = spark.read.format('xml')\
+                     .options(rowTag='row')\
+                     .options(rootTag='postlinks')\
+                     .load(PATHS['postlinks_xml'][PATH_SET])
+
+post_links_df.write\
+             .mode('overwrite')\
+             .parquet(PATHS['postlinks'][PATH_SET])
