a4895bb29a990f98cf94967d63a815a085dc9629
Russell Jurney <russell.jurney@gmail.com>
2020-08-03 21:01:42 -0700

    Updated EMR bootstrap script to work with latest EMR/Amazon Linux

--- ch02/emr_bootstrap.sh
+++ ch02/emr_bootstrap.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -x -e
 
+# Setup Python
+sudo yum -y install python-devel
+
 # Install all required modules
-sudo pip-3.6 install lxml frozendict ipython pandas boto3 bs4 nltk 
+sudo `which pip3` install lxml frozendict ipython pandas boto3 bs4 nltk 
 
 # Download nltk data
 python3 -m nltk.downloader punkt
 python3 -m nltk.downloader stopwords
 
-# Install Mosh
+# Install Mosh for long running ssh that won't die
 sudo yum -y install mosh git
 
 # Install requirements for Snorkel processing
-sudo pip-3.6 install beautifulsoup4 dill gensim iso8601 jupyter numpy>=1.16.0 pandas pip-tools pyarrow>=0.16.0 requests s3fs scikit-learn spacy textblob textdistance texttable
-
-# Install snorkel master
-sudo pip-3.6 install -e git+git://github.com/snorkel-team/snorkel@master#egg=snorkel 
+sudo `which pip3` install beautifulsoup4 dill gensim iso8601 jupyter numpy pandas<0.26.0 pip-tools pyarrow requests s3fs scikit-learn<0.22.0 snorkel spacy textblob textdistance texttable
 
 # Download another model
 sudo python3 -m spacy download en_core_web_lg
